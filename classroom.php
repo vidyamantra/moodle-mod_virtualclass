8a68ba41d7ab89f361eb185cd387fa7bec4b73d3
Suman <suman@dev.vidya.io>
2015-02-04 11:58:49 +0530

    validated the code by codechecker

--- classroom.php
+++ classroom.php
@@ -54,17 +54,17 @@ $event = \mod_virtualclass\event\course_module_viewed::create(array(
 ));
 $event->add_record_snapshot('course', $PAGE->course);
 // In the next line you can use $PAGE->activityrecord if you have set it, or skip this line if you don't have a record.
-//$event->add_record_snapshot($PAGE->cm->modname, $activityrecord);
+// $event->add_record_snapshot($PAGE->cm->modname, $activityrecord);.
 $event->trigger();
 
 // Print the page header.
 $PAGE->set_url('/mod/virtualclass/view.php', array('id' => $cm->id));
-$PAGE->set_popup_notification_allowed(false); // No popup notifications in virtual classroom
+$PAGE->set_popup_notification_allowed(false); // No popup notifications in virtual classroom.
 $PAGE->set_title(format_string($virtualclass->name));
-//$PAGE->set_heading(format_string($course->fullname));
+// $PAGE->set_heading(format_string($course->fullname));.
 $PAGE->set_context($context);
 
-//$PAGE->set_pagelayout('embedded');
+// $PAGE->set_pagelayout('embedded');.
 $PAGE->set_pagelayout('popup');
 $PAGE->requires->jquery(true);
 $PAGE->requires->jquery_plugin('ui');
@@ -78,20 +78,21 @@ $PAGE->requires->css(new moodle_url($CFG->wwwroot . '/mod/virtualclass/bundle/vi
 <link rel="chrome-webstore-item" href="https://chrome.google.com/webstore/detail/ijhofagnokdeoghaohcekchijfeffbjl">
 <?php
 
-// Mark viewed by user (if required)
+// Mark viewed by user (if required).
 $completion = new completion_info($course);
 $completion->set_module_viewed($cm);
 
 // Checking moodle deugger is on or not.
 $info = 0;
-//for now we disabling the packet container
-//if($CFG->debug == 32767 && $CFG->debugdisplay == 1){
-//    $info = 1;
-//}
+// For now we disabling the packet container.
+/* if($CFG->debug == 32767 && $CFG->debugdisplay == 1){
+        $info = 1;
+    }
+ */
 $whiteboard_path = $CFG->wwwroot . "/mod/virtualclass/bundle/virtualclass/";
 $sid = $USER->sesskey;
 
-$r = 's'; //default role
+$r = 's'; // Default role.
 if (has_capability('mod/virtualclass:addinstance', $context)) {
     if ($USER->id == $virtualclass->moderatorid) {
         $r = 't';
@@ -102,33 +103,24 @@ if (has_capability('mod/virtualclass:addinstance', $context)) {
 echo $OUTPUT->header();
 ?>
 <script type="text/javascript">
-    <?php //echo "name='".$USER->username."';"; ?>
-    <?php //echo "id='".$USER->id."';"; ?>
-    <?php //echo "sid='".$sid."';";?>
-    <?php //echo "fname='".$USER->firstname."';"; ?>
-    <?php //echo "lname='".$USER->lastname."';"; ?>
-    
-    <?php 
-        if ($USER->id) {
-            $userpicture = moodle_url::make_pluginfile_url(context_user::instance($USER->id)->id, 'user', 'icon', null, '/', 'f2');
-            $src = $userpicture->out(false);
-        }else{
-             
-            $src = 'bundle/virtualclass/images/quality-support.png';
-        }
-        echo "wbUser.imageurl='".$src."';";
-    ?>
-        
-    <?php echo "wbUser.name='".$USER->firstname."';"; ?>
-    <?php echo "wbUser.id='".$USER->id."';"; ?>
-    <?php echo "wbUser.socketOn='$info';"; ?>
-    <?php echo "wbUser.dataInfo='$info';"; ?>
-    <?php echo "wbUser.room='" . $course->id . "_" . $cm->id."';"; ?>
-    <?php echo "wbUser.sid='".$sid."';"; ?>
-    <?php echo "wbUser.role='".$r."';"; ?>
-    <?php echo "wbUser.fname='".$USER->firstname."';"; ?>
-    <?php echo "wbUser.lname='".$USER->lastname."';"; ?>
-
+<?php 
+if ($USER->id) {
+    $userpicture = moodle_url::make_pluginfile_url(context_user::instance($USER->id)->id, 'user', 'icon', null, '/', 'f2');
+    $src = $userpicture->out(false);
+} else {
+    $src = 'bundle/virtualclass/images/quality-support.png';
+}
+    echo "wbUser.imageurl='".$src."';";
+    echo "wbUser.name='".$USER->firstname."';";
+    echo "wbUser.id='".$USER->id."';";
+    echo "wbUser.socketOn='$info';";
+    echo "wbUser.dataInfo='$info';";
+    echo "wbUser.room='" . $course->id . "_" . $cm->id."';";
+    echo "wbUser.sid='".$sid."';";
+    echo "wbUser.role='".$r."';";
+    echo "wbUser.fname='".$USER->firstname."';";
+    echo "wbUser.lname='".$USER->lastname."';";
+?>
     window.whiteboardPath =  '<?php echo $whiteboard_path; ?>';
     if (!!window.Worker) {
         var sworker = new Worker("<?php echo $whiteboard_path."src/screenworker.js" ?>");
@@ -136,12 +128,12 @@ echo $OUTPUT->header();
 </script>
  
 <?php
-//require_once('bundle/virtualclass/build/js.debug.php');
+// require_once('bundle/virtualclass/build/js.debug.php');.
 
 $PAGE->requires->js('/mod/virtualclass/bundle/virtualclass/bundle/io/src/iolib.js');
 $PAGE->requires->js('/mod/virtualclass/bundle/virtualclass/build/wb.min.js');
 
-//$PAGE->requires->js('/mod/virtualclass/bundle/virtualclass/build/chat.min.js');
+// $PAGE->requires->js('/mod/virtualclass/bundle/virtualclass/build/chat.min.js');.
 $PAGE->requires->js('/mod/virtualclass/bundle/virtualclass/index.js');
 
 if ($r == 't') {
@@ -163,49 +155,33 @@ echo html_writer::start_tag('div', array('id' => 'vAppCont', 'class' => "$role")
 
 echo html_writer::start_tag('div', array('id' => 'audioWidget'));
 
-    if ($r == 's') {
-        $dap = "false";
-        $classes = "audioTool deactive";
-        echo html_writer::start_tag('div', array('id' => 'speakerStudent'));
+if ($r == 's') {
+    $dap = "false";
+    $classes = "audioTool deactive";
+    echo html_writer::start_tag('div', array('id' => 'speakerStudent'));
 
-            echo html_writer::start_tag('div', array('id' => 'speakerPressing', 'class' => 'audioTool deactive' ));
-                //echo html_writer::tag('canvas', '', array('id' => 'speakerPressingImg', 'width' => 40, 'height' => 40));
-                echo html_writer::start_tag('a', array('id' => 'speakerPressingAnch', 'class' => 'tooltip', 'data-title' => get_string('pressalways', 'virtualclass')));
-//                    $iconurl = new moodle_url('http://web.icq.com/whitepages/online', array('icq' => $user->icq, 'img' => '5'));
-//                    $statusicon = html_writer::tag('img', '', array('src' => $iconurl, 'class' => 'icon icon-post', 'alt' => get_string('status')));
-                    $pressing_img = $whiteboard_path . "images/speakerpressing.png";
-                    echo html_writer::tag('img', '', array('id' => 'speakerPressingImg', 'src' => $pressing_img));
+        echo html_writer::start_tag('div', array('id' => 'speakerPressing', 'class' => 'audioTool deactive' ));
+            // echo html_writer::tag('canvas', '', array('id' => 'speakerPressingImg', 'width' => 40, 'height' => 40));.
+            echo html_writer::start_tag('a', array('id' => 'speakerPressingAnch', 'class' => 'tooltip', 'data-title' => get_string('pressalways', 'virtualclass')));
+    // $iconurl = new moodle_url('http://web.icq.com/whitepages/online', array('icq' => $user->icq, 'img' => '5'));.
+    // $statusicon = html_writer::tag('img', '', array('src' => $iconurl, 'class' => 'icon icon-post', 'alt' => get_string('status')));.
+                $pressingimg = $whiteboard_path . "images/speakerpressing.png";
+                echo html_writer::tag('img', '', array('id' => 'speakerPressingImg', 'src' => $pressingimg));
 
-                echo html_writer::end_tag('a');
+            echo html_writer::end_tag('a');
 
-            echo html_writer::end_tag('div');
         echo html_writer::end_tag('div');
-    }else{
-          $classes = "audioTool active";
-          $dap = "true";
-    }
-    
-    $pressonce_img = $whiteboard_path . "images/speakerpressonce.png";
+    echo html_writer::end_tag('div');
+} else {
+      $classes = "audioTool active";
+      $dap = "true";
+}
+    $pressonceimg = $whiteboard_path . "images/speakerpressonce.png";
     echo html_writer::start_tag('div', array('id' => 'speakerPressOnce', 'class' => $classes, 'data-audio-playing' => $dap));
         echo html_writer::start_tag('a', array('id' => 'speakerPressonceAnch', 'class' => 'tooltip', 'data-title' => get_string('pressonce', 'virtualclass') ));
-             echo html_writer::tag('img', '', array('id' => 'speakerPressonceImg', 'src' => $pressonce_img));
+             echo html_writer::tag('img', '', array('id' => 'speakerPressonceImg', 'src' => $pressonceimg));
         echo html_writer::end_tag('a');
     echo html_writer::end_tag('div');
-
-//    echo html_writer::start_tag('div', array('id' => 'speakerPressOnce', 'class' => 'audioTool'));
-//        $pressonce_img = $whiteboard_path . "images/speakerpressonce.png";
-//        echo html_writer::start_tag('a', array('id' => 'speakerPressonceAnch'));
-//             echo html_writer::tag('img', '', array('id' => 'speakerPressonceImg', 'src' => $pressonce_img, 'width' => 40, 'height' => 40));
-//        echo html_writer::end_tag('a');
-//    echo html_writer::end_tag('div');
-
-//    echo html_writer::start_tag('div', array('id' => 'audioTest', 'class' => 'audioTool'));
-//        $audioimg = $whiteboard_path . "images/audiotest.png";
-//        echo html_writer::start_tag('a', array('id' => 'audiotestAnch', 'classs' => 'tooltip', 'data-title' => get_string('audiotest', 'virtualclass')));
-//             echo html_writer::tag('img', '', array('id' => 'audiotestImg', 'src' => $audioimg));
-//        echo html_writer::end_tag('a');
-//    echo html_writer::end_tag('div');
-//    
 
     echo html_writer::start_tag('div', array('id' => 'audioTest', 'class' => 'audioTool'));
         $audioimg = $whiteboard_path . "images/audiotest.png";
