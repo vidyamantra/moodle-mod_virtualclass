d4a0158b2f82bb1c1091fbb15dd5348ae744f745
Pinky Sharma <pinky@vidyamantra.com>
2015-12-03 16:13:40 +0530

    version update

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015111200;      // The current module version (Date: YYYYMMDDXX).
-$plugin->release = '2.0 (Build: 2015111200)';
+$plugin->release = '1.3 (Build: 2015111200)';
 $plugin->requires  = 2014051202;      // Requires this Moodle version.(2.7.2+)
 $plugin->cron      = 0;               // Period for cron to check this module (secs).
 $plugin->component = 'mod_virtualclass'; // To check on upgrade, that module sits in correct place.
